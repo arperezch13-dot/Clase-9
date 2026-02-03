65f7c2fbc31950c8b54f2770a8c67da05ad16971
Full Stack <fullstack@Valencia-Aula-9.local>
2026-02-03 20:56:15 +0100

    Primer commit

---
+++ practica1.js
@@ -0,0 +1,8 @@
+// Ejercicio 1: El entorno de pruebas 
+// Objetivo: Comprender que lo que pasa en una rama, se queda en esa rama hasta que decidamos lo contrario.
+// Asegúrate de estar en tu repositorio y en la rama principal (main).
+// Crea una nueva rama llamada test-experimental y cámbiate a ella con el comando: git checkout -b test-experimental
+// Crea un archivo llamado experimento.txt y escribe algo dentro.
+// Haz un commit con los cambios: git add . git commit -m "Probando cosas nuevas"
+// Ahora, regresa a la rama principal: git checkout main
+// Pregunta de reflexión: ¿Ves el archivo experimento.txt en tu carpeta?
